5194ccfb96c65d8dcd348497326c714479126ad8
Petr Nagy <github@petrnagy.cz>
2026-06-24 13:11:24 +0200

    chore: allow local home page

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -5,8 +5,8 @@ import { env } from "next-runtime-env";
 export function middleware(request: NextRequest) {
   if (request.nextUrl.pathname === "/") {
     if (env("NEXT_PUBLIC_KAN_ENV") !== "cloud") {
-      const loginUrl = new URL("/login", request.url);
-      return NextResponse.redirect(loginUrl);
+      // const loginUrl = new URL("/login", request.url);
+      // return NextResponse.redirect(loginUrl);
     }
   }
 
